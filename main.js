8bf13ab3431a751e61ea4fff342df7924334b8f2
RIANPURNAMA1 <rianprnma7@gmail.com>
2023-06-06 15:41:10 +0700

    perubahan pada javascrip

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function sendMessage() {
     var name = document.getElementById('name').value
     var message = document.getElementById('message').value
-    var whatsapp = '6285161363585'
+    var whatsapp = '6282118364415'
 
     var url = "https://api.whatsapp.com/send?phone=" + whatsapp + "&text=Halo, nama saya " + name + ". " + message 
     window.open(url)
